3a2f61c240bdcdb708c4f1e21e9454d1eb1ba5e7
phuc.pham <nngo2@slb.com>
2024-12-13 15:09:04 +0700

    Update new code 2

--- app/module/roles/router/main.router.go
+++ app/module/roles/router/main.router.go
@@ -2,7 +2,6 @@ package user_roles_router
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/phucpham-infinity/go-nextpress/app/middlewares"
 	roles_controllers "github.com/phucpham-infinity/go-nextpress/app/module/roles/controllers"
 )
 
@@ -10,11 +9,19 @@ func NewUserRolesRouter(v1 *fiber.Router) {
 	rolesController := roles_controllers.NewRolesController()
 	rolesRouter := (*v1).Group("/roles")
 
-	rolesRouter.Get("/", middlewares.AuthenticationMiddleware(), rolesController.GetAllRoles)
-	rolesRouter.Get("/:id", middlewares.AuthenticationMiddleware(), rolesController.GetByIdRoles)
+	//rolesRouter.Get("/", middlewares.AuthenticationMiddleware(), rolesController.GetAllRoles)
+	//rolesRouter.Get("/:id", middlewares.AuthenticationMiddleware(), rolesController.GetByIdRoles)
+	//
+	//rolesRouter.Post("/", middlewares.AuthenticationMiddleware(), rolesController.CreateRoles)
+	//rolesRouter.Put("/", middlewares.AuthenticationMiddleware(), rolesController.UpdateRoles)
+	//
+	//rolesRouter.Delete("/:id", middlewares.AuthenticationMiddleware(), rolesController.DeleteRolesById)
 
-	rolesRouter.Post("/", middlewares.AuthenticationMiddleware(), rolesController.CreateRoles)
-	rolesRouter.Put("/", middlewares.AuthenticationMiddleware(), rolesController.UpdateRoles)
+	rolesRouter.Get("/", rolesController.GetAllRoles)
+	rolesRouter.Get("/:id", rolesController.GetByIdRoles)
 
-	rolesRouter.Delete("/:id", middlewares.AuthenticationMiddleware(), rolesController.DeleteRolesById)
+	rolesRouter.Post("/", rolesController.CreateRoles)
+	rolesRouter.Put("/", rolesController.UpdateRoles)
+
+	rolesRouter.Delete("/:id", rolesController.DeleteRolesById)
 }
